b922f6ceff224cecf29048049b9263b2eb8358ac
A-Olatunbosun11 <105355136+A-Olatunbosun11@users.noreply.github.com>
2022-06-24 00:01:31 +0100

    First Commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let myName = 'My Name is Adeoye Olatunbosun';
+let myHeight = 'I am 5FT 11inches Tall';
+let myCountry = 'I am from Nigeria';
+
+console.log(myName);
+console.log(myHeight);
+console.log(myCountry);
+
+// alert(myName);
+// alert(myHeight);
+// alert(myCountry);
